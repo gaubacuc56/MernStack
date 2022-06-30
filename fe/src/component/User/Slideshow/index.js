6abfb8b5796410c2f7e2c035043a7b8e694fbb35
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-06-30 15:17:01 +0700

    implement slideshow1 with fully responsive

--- fe/src/component/User/Slideshow/index.js
+++ fe/src/component/User/Slideshow/index.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+
+import slide1 from "../../../assets/img/slide1.jpg";
+import slide2 from "../../../assets/img/slide2.jpg";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import style from "./slideshow.module.css";
 
 export default function Slideshow() {
-    return (
-        <div>index</div>
-    )
+  return (
+    <div className={`${style.slideShow1} grid d-flex`}>
+      <Swiper slidesPerView={1} spaceBetween={30} loop={true}>
+        <SwiperSlide>
+          <img src={slide1}> </img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2}></img>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
--- fe/src/layout/User/Layout.js
+++ fe/src/layout/User/Layout.js
@@ -3,11 +3,13 @@ import Header from '../../component/User/Header'
 import Footer from '../../component/User/Footer'
 import Navigation from '../../component/User/Navigation'
 import { Outlet } from 'react-router-dom'
+import Slideshow from '../../component/User/Slideshow'
 export default function Layout() {
     return (
         <>
             <Header></Header>
             <Navigation></Navigation>
+            <Slideshow></Slideshow>
             <Outlet />
             <Footer></Footer>
         </>
