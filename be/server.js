bcc44fe6c8ac51ece352af0557dd77deb241121e
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-07-05 11:16:14 +0700

    push

--- be/server.js
+++ be/server.js
@@ -10,7 +10,7 @@ const dotenv = require("dotenv");
 const productRoute = require("./routes/product");
 
 /* Define Port */
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 /* connect database */
