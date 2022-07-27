a1a7d07546a07907e162e910622adb954423ad39
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-07-27 17:08:19 +0700

    fix: wrong route in getproductbyvalue

--- routes/product.js
+++ routes/product.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.post("/newProduct", productControllers.addProduct);
 router.get("/getAll", productControllers.getAllProduct);
 router.get("/getAProduct/:id", productControllers.getAProduct);
+router.get("/getProductByValue/:value", productControllers.getProductByValue);
 router.put("/updateAProduct/:id", productControllers.updateAProduct);
 module.exports = router;
