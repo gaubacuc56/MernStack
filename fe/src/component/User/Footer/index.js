2f00a5b227aca6d59c82708f29b50027165e7b5b
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-08-14 20:33:26 +0700

    fix: fix footer

--- fe/src/component/User/Footer/index.js
+++ fe/src/component/User/Footer/index.js
@@ -11,44 +11,46 @@ export default function Footer() {
     }
     return (
         <div className={style.footer}>
-            <div className={`${style.footer_container} grid row justify-content-between`} >
-                <div className={`${style.footer_container_item} ${style.contact} 	col-lg-3  col-sm-6 col-12`} >
-                    <h3>{t('FOOTER.CONTACT.TITLE')}</h3>
-                    <div className={style.separate}></div>
-                    <p>{t('FOOTER.CONTACT.ADDRESS')}</p>
-                    <p>Phone: 0938559501</p>
-                    <p >Email: uit.edu.vn@gmail.com</p>
-                </div>
-                <div className={`${style.footer_container_item} ${style.policy} col-lg-3  col-sm-6 col-12`}  >
-                    <h3>{t('FOOTER.POLICY.TITLE')}</h3>
-                    <div className={style.separate}></div>
-                    <a href="#">{t('FOOTER.POLICY.SWAP')}</a>
-                    <a href="#">{t('FOOTER.POLICY.INSTALMENT')}</a>
-                    <a href="#">{t('FOOTER.POLICY.CARE')}</a>
-                    <a href="#">{t('FOOTER.POLICY.DELIVERY')}</a>
-                </div>
-                <div className={`${style.footer_container_item}  ${style.link}  col-lg-3  col-sm-6 col-12`} >
-                    <h3>{t('FOOTER.LINK.TITLE')}</h3>
-                    <div className={style.separate}></div>
-                    <p>{t('FOOTER.LINK.DESC')}</p>
-                    <div className={style.social_flatform}>
-                        <i className="fab fa-facebook-f"></i>
-                        <i className="fab fa-twitter"></i>
-                        <i className="fab fa-google-plus-g"></i>
-                        <i className="fab fa-pinterest"></i>
+            <div className='grid'>
+                <div className={`row justify-content-between`} >
+                    <div className={`${style.footer_container_item} ${style.contact} 	col-lg-3  col-sm-6 col-12`} >
+                        <h3>{t('FOOTER.CONTACT.TITLE')}</h3>
+                        <div className={style.separate}></div>
+                        <p>{t('FOOTER.CONTACT.ADDRESS')}</p>
+                        <p>Phone: 0938559501</p>
+                        <p >Email: uit.edu.vn@gmail.com</p>
                     </div>
-                    {/*    <img src="/img/dkbocongthuong.png"></img> */}
-                </div>
-                <div className={`${style.footer_container_item}  ${style.transfers}  col-lg-3  col-sm-6 col-12`} >
-                    <h3>{t('FOOTER.BANK.TITLE')}</h3>
-                    <div className={style.separate}></div>
-                    <div className={style.transfers_info}>
-                        <p>BIDV:   1331 00000 74767</p>
-                        <p>Momo:   0934 189 092</p>
-                        <p>Zalopay:   0934 189 092</p>
-                        <p>Payoo:   0934 189 092</p>
+                    <div className={`${style.footer_container_item} ${style.policy} col-lg-3  col-sm-6 col-12`}  >
+                        <h3>{t('FOOTER.POLICY.TITLE')}</h3>
+                        <div className={style.separate}></div>
+                        <a href="#">{t('FOOTER.POLICY.SWAP')}</a>
+                        <a href="#">{t('FOOTER.POLICY.INSTALMENT')}</a>
+                        <a href="#">{t('FOOTER.POLICY.CARE')}</a>
+                        <a href="#">{t('FOOTER.POLICY.DELIVERY')}</a>
                     </div>
+                    <div className={`${style.footer_container_item}  ${style.link}  col-lg-3  col-sm-6 col-12`} >
+                        <h3>{t('FOOTER.LINK.TITLE')}</h3>
+                        <div className={style.separate}></div>
+                        <p>{t('FOOTER.LINK.DESC')}</p>
+                        <div className={style.social_flatform}>
+                            <i className="fab fa-facebook-f"></i>
+                            <i className="fab fa-twitter"></i>
+                            <i className="fab fa-google-plus-g"></i>
+                            <i className="fab fa-pinterest"></i>
+                        </div>
+                        {/*    <img src="/img/dkbocongthuong.png"></img> */}
+                    </div>
+                    <div className={`${style.footer_container_item}  ${style.transfers}  col-lg-3  col-sm-6 col-12`} >
+                        <h3>{t('FOOTER.BANK.TITLE')}</h3>
+                        <div className={style.separate}></div>
+                        <div className={style.transfers_info}>
+                            <p>BIDV:   1331 00000 74767</p>
+                            <p>Momo:   0934 189 092</p>
+                            <p>Zalopay:   0934 189 092</p>
+                            <p>Payoo:   0934 189 092</p>
+                        </div>
 
+                    </div>
                 </div>
             </div>
             <div className={style.longSeparate}></div>
